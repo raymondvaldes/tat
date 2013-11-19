0712ea2d953b374d09106da37e0ecc592fa80ff3
Raymond Valdes <raymondvaldes@gmail.com>
2013-11-18 17:28:06 -0800

    Created object and namespace reference
    class numericalModel::simulations numSimu

--- models/poptea.hpp
+++ models/poptea.hpp
@@ -15,12 +15,8 @@ namespace thermalAnalysisMethod
     class expEquipment::setup expSetup;
     class physicalModel::TBCsystem TBCsystem;
     class ThermalModelSelection thermalModel;
-
-//    struct parameterEstimation::settings Settings;
-//    class parameterEstimation::unknownList unknownParameters;
-//    class parameterEstimation::LMA_workingArrays LMA_workspace;
-
     class parameterEstimation::LMA LMA;
+    class numericalModel::simulations numSimu;
 
     PopTea(class expEquipment::setup expSetup_,
            class physicalModel::TBCsystem TBCsystem_,
