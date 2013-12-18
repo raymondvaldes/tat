d6d5417bcc8e662962dd3c7e480260398110c4c6
Raymond Valdes <raymondvaldes@gmail.com>
2013-12-18 14:00:06 -0800

    GPL3
    Added GPL3 license and copyright to all source files and headers.

--- src/numSimulations/Numerical_PhaseOfEmission.cpp
+++ src/numSimulations/Numerical_PhaseOfEmission.cpp
@@ -1,3 +1,27 @@
+/*----------------------------------------------------------------------------*\
+  ========                |
+     || 	 T Thermal      | TAT: Thermal Analysis Toolbox
+     ||  	 A Analysis     |        
+     || 	 T Toolbox    	| Copyright (C) 2013 Raymond Valdes
+     ||   	  						|
+-------------------------------------------------------------------------------
+License                                                                         
+	  This file is part of Thermal Analysis Toolbox.
+
+    Thermal Analysis Toolbox is free software: you can redistribute it and/or 
+ 	  modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your 
+	  option) any later version.
+
+    Thermal Analysis Toolbox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with 
+	  Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
+
+\*----------------------------------------------------------------------------*/
 #include "models/physicalmodel.hpp"
 #include "numSimulations/Numerical_PhaseOfEmission.h"
 #include "algorithms/statistical_tools.hpp"
