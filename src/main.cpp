187a3baa7f5bb4aae502797b936073a974be90a9
Raymond Valdes <raymondvaldes@gmail.com>
2013-12-18 14:16:35 -0800

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 /*----------------------------------------------------------------------------*\
-  ========      		      |
+  ========      	        |
      || 	 T Thermal      | TAT: Thermal Analysis Toolbox
      ||  	 A Analysis     |
      || 	 T Toolbox    	| Copyright (C) 2013 Raymond Valdes
-     ||  		           	  |
+     ||  		        |
 -------------------------------------------------------------------------------
 License                                                                         
     This file is part of Thermal Analysis Toolbox.
@@ -15,8 +15,8 @@ License
 
     Thermal Analysis Toolbox is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+    Public License for more details.
 
     You should have received a copy of the GNU General Public License along with 
     Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
