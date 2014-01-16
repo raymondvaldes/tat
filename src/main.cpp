ee7d9088aa733739e37b8385e132afb38a816fa5
Raymond Valdes <raymondvaldes@gmail.com>
2014-01-16 14:04:19 -0800

    removed command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ License
 \*----------------------------------------------------------------------------*/
 #include "Header.h"
 
-int main( int /*argc*/, char* argv[] )
+int main( int /*argc*/, char* /*argv[]*/ )
 {
   std::cout << "Welcome back, Raymond!\n\n";
   class stopwatch globalStopWatch;
