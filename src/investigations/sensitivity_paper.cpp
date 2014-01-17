21a1b7941a05f21ca2caec893aaf2575edc2f129
Raymond Valdes <raymondvaldes@gmail.com>
2014-01-17 15:25:23 -0800

    formatted license

--- src/investigations/sensitivity_paper.cpp
+++ src/investigations/sensitivity_paper.cpp
@@ -1,16 +1,16 @@
 /*----------------------------------------------------------------------------*\
-  ========                  |
+  ========                      |
      || 	 T Thermal      | TAT: Thermal Analysis Toolbox
-     ||  	 A Analysis     |        
+     ||  	 A Analysis     |
      || 	 T Toolbox    	| Copyright (C) 2013 Raymond Valdes
-     ||   	  				|
+     ||   	  		|
 -------------------------------------------------------------------------------
-License                                                                         
-	  This file is part of Thermal Analysis Toolbox.
+License
+    This file is part of Thermal Analysis Toolbox.
 
-    Thermal Analysis Toolbox is free software: you can redistribute it and/or 
+    Thermal Analysis Toolbox is free software: you can redistribute it and/or
     modify it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or (at your 
+    the Free Software Foundation, either version 3 of the License, or (at your
     option) any later version.
 
     Thermal Analysis Toolbox is distributed in the hope that it will be useful,
@@ -18,7 +18,7 @@ License
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along with 
+    You should have received a copy of the GNU General Public License along with
     Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
 
 \*----------------------------------------------------------------------------*/
@@ -30,6 +30,7 @@ namespace SensitivityValdes2013
 This namespace will hold all the functions necessary to reproduce the work
 in the Valdes2013 Sensitivity Paper.
 */
+
 ///Parameter Estimation Options
 /* - N number of thermal parameters to be fitted
  - ftol difference in error
--- src/investigations/sensitivityvaldes2013.hpp
+++ src/investigations/sensitivityvaldes2013.hpp
@@ -22,37 +22,13 @@ License
     Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
 
 \*----------------------------------------------------------------------------*/
-#include "../Header.h"
+#ifndef SENSITIVITYVALDES2013_HPP
+#define SENSITIVITYVALDES2013_HPP
 
-namespace filesystem
+class sensitivityValdes2013
 {
+public:
+  sensitivityValdes2013();
+};
 
-void makeDir(const std::string rootPath, const std::string newDirectory)
-{
-    boost::filesystem::path directory(rootPath + "/" +newDirectory);
-    boost::filesystem::create_directory(directory);
-    return;
-}
-
-
-directory::directory(const std::string workingDirectory_):
-  workingDirectory(workingDirectory_){}
-
-directory::~directory(void){}
-void directory::mkdir(const std::string newDirectory) const
-{
-  makeDir(workingDirectory,newDirectory);
-}
-
-std::string directory::pwd(void) const
-{
-  return workingDirectory;
-}
-
-std::string directory::abs(const std::string relativePath) const
-{
-  return workingDirectory + "/" + relativePath;
-}
-
-
-}
+#endif // SENSITIVITYVALDES2013_HPP
