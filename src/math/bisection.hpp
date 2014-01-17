4cfc9740f27b1d1924d7b473771c922fa9f09bbd
Raymond Valdes <raymondvaldes@gmail.com>
2014-01-17 15:13:31 -0800

    added copyleft

--- src/math/bisection.hpp
+++ src/math/bisection.hpp
@@ -25,10 +25,40 @@ License
 #include <boost/math/tools/roots.hpp>
 #include <functional>
 
+/*----------------------------------------------------------------------------*\
+  ========                      |
+     || 	 T Thermal      | TAT: Thermal Analysis Toolbox
+     ||  	 A Analysis     |
+     || 	 T Toolbox    	| Copyright (C) 2013 Raymond Valdes
+     ||   	  		|
+-------------------------------------------------------------------------------
+License
+    This file is part of Thermal Analysis Toolbox.
+
+    Thermal Analysis Toolbox is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    Thermal Analysis Toolbox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with
+    Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
+
+\*----------------------------------------------------------------------------*/
+
 #ifndef BISECTION_HPP_INCLUDED
 #define BISECTION_HPP_INCLUDED
-/*This class solves the function:                        f(x) = phi
-  by using the bisection method to solve :   f(x) - phi  =  0
+
+/*  This class solves the function:
+ *  f(x) = phi
+ *  by using the bisection method to solve :   f(x) - phi  =  0.
+ *
+ *  Use of this class is simplified using the class interface. Please feed it
+ *  function with a given range
 */
 
 namespace math{
