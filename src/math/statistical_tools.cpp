cf21b3222517a39b5f8d5da978b3471a8dccfa0c
Raymond Valdes <raymondvaldes@gmail.com>
2014-01-27 16:24:13 -0800

    moved some files

---
+++ src/math/statistical_tools.cpp
@@ -0,0 +1,427 @@
+/*----------------------------------------------------------------------------*\
+  ========                      |
+     || 	 T Thermal      | TAT: Thermal Analysis Toolbox
+     ||  	 A Analysis     |
+     || 	 T Toolbox    	| Copyright (C) 2013 Raymond Valdes
+     ||   	  		|
+-------------------------------------------------------------------------------
+License
+    This file is part of Thermal Analysis Toolbox.
+
+    Thermal Analysis Toolbox is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    Thermal Analysis Toolbox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with
+    Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
+
+\*----------------------------------------------------------------------------*/
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <cstddef>
+#include <iostream>
+
+#include "math/numIntegration/gslfunc.hpp"
+
+void vector3DSetup(std::vector< std::vector< std::vector< double > > > &Vnew,
+                   const size_t x, const size_t y, const size_t z)
+{
+  Vnew.resize(x);
+  for(size_t i = 0; i < x ; ++i )
+  {
+    Vnew[i].resize(y);
+
+    for(size_t j = 0; j < y ; ++j)
+    {
+       Vnew[i][j].resize(z);
+    }
+  }
+  return;
+}
+
+void vector2DSetup(std::vector< std::vector< double > > &Vnew,
+                   const size_t x, const size_t y)
+{
+    Vnew.resize(x);
+    for(size_t i = 0; i < x ; ++i )
+    {
+        Vnew[i].resize(y);
+    }
+}
+
+double rss(const double N, const double* exp, const double* x_hat)
+{
+    /*
+        This function calculated the residual sum of squares between an
+        experimental results and predicted results.
+            exp         experimental results
+            x_hat       predicted results
+            N           number of measurements
+    */
+    double sum = 0;
+    for(size_t n = 0; n < N ; n++)
+    {
+        sum += (exp[n] - x_hat[n]) * (exp[n] - x_hat[n]);
+    }
+
+    return sum;
+}
+
+double variance(const size_t P, const size_t N, const double S)
+{
+    /*
+        The estimate of the variance (sigma^2)  is obtained from the degrees of
+        freedom and the objective function.
+            S ... objective function
+            P ... number of measurements
+            N ... number of unknown parameters
+    */
+    return S / (P-N);
+}
+
+double chisquare(const size_t N, const double*emissionExp,
+                    const double* emissionEst)
+{
+    /*
+        The objective function is evaluated for all the measurements.
+        emissionEst[n]  ...     is calculated through the model
+        emissionExp[n]  ...     is determined through the experiment
+    */
+
+    double chisquare = 0;
+
+    for(size_t n=0 ; n < N; n++)
+    {
+        chisquare += (pow( emissionExp[n] - emissionEst[n]  , 2) / emissionEst[n]);
+    }
+    return chisquare;
+}
+
+double Sobjective(const size_t L_end, const double*emissionExp,
+                  const double* emissionNum)
+{
+    /*
+        The objective function is evaluated for all the measurements and the
+        mean square is returned.
+
+    */
+    double fvec_objective = 0;
+
+    for(size_t n=0 ; n < L_end; n++)
+    {
+        fvec_objective += pow((emissionExp[n] / emissionNum[n] - 1), 2);
+    }
+
+    return sqrt(fvec_objective / L_end );
+}
+
+double MSE(const size_t N, std::vector<double> &emissionExp,
+           std::vector<double> &emissionEst)
+{
+    /*
+        The objective function is evaluated for all the measurements and the
+        mean square is returned.
+    */
+    double fvec_objective = 0;
+
+    for(size_t n=0 ; n < N; n++)
+    {
+        fvec_objective += pow( emissionEst[n]/emissionExp[n] - 1  , 2);
+    }
+
+    return 100*(fvec_objective / N);
+}
+
+double MSE(const size_t N, const double* const emissionExp,
+           const double* const emissionEst)
+{
+    /*
+        The objective function is evaluated for all the measurements and the
+        mean square is returned.
+
+    */
+    double fvec_objective = 0;
+
+    for(size_t n=0 ; n < N; n++)
+    {
+        fvec_objective += pow( emissionEst[n]/emissionExp[n] - 1  , 2);
+    }
+
+    return 100*(fvec_objective / N);
+}
+
+void matrixTranspose(const size_t P, const size_t N, double*matrix)
+{
+    /*
+       N is parameters being estimated
+       P number of measurements
+    */
+
+    for(size_t j = 0 ; j < P ; ++j)
+    {
+        for(size_t i = 0 ; i < N ; ++i)
+        {
+            matrix[j+P*i] = matrix[i+j*N];
+        }
+    }
+
+    return;
+}
+
+void matrixTranspose(const size_t P, const size_t N, const double*matrix,
+                     double*matrixT)
+{
+    /*
+       N is parameters being estimated
+       P number of measurements
+    */
+
+    for(size_t j = 0 ; j < P ; ++j)
+    {
+        for(size_t i = 0 ; i < N ; ++i)
+        {
+            matrixT[j+P*i] = matrix[i+j*N];
+        }
+    }
+
+    return;
+}
+
+void matrixDot(const double* MatrixA, const size_t rowA, const size_t columnA,
+               const double* MatrixB, const size_t rowB, const size_t columnB,
+               double* MatrixResultant)
+{
+    /*
+        This is for matrices arranged in 1D with the indexes such that:
+            i + m * j
+                i column
+                j row
+                m row width
+
+        The arguments row and column are the lengths of the row and column of the
+        resultant matrix.
+            MatrixResultant[row,column] = MatrixA[row,::] * MatrixB[::,column]
+     */
+    if(columnA != rowB)
+    {
+        std::cout << "\nerror with matrix size\n";
+        return;
+    }
+
+    for(size_t j = 0 ; j < rowA ; ++j)
+    {
+        for(size_t i = 0 ; i < columnB ; ++i)
+        {
+            MatrixResultant[i+columnB*j] = 0;
+
+            for (size_t k = 0; k < columnA; ++k)
+            {
+                MatrixResultant[i+columnB*j] +=
+                    MatrixA[k + j*columnA] * MatrixB[i + columnB*k];
+            }
+        }
+    }
+
+    return;
+}
+
+void covarianceMatrix(const size_t N, const double variance,
+                      const double* Amatrix, double* cov)
+{
+    /*
+        The covariance matrix is the product of the variance and the jacobian
+        matrix.
+    */
+    double* AmatrixT;
+    AmatrixT = (double*)malloc(N * N * sizeof(double));
+    matrixTranspose(N, N, Amatrix, AmatrixT);
+
+    size_t ij=0;
+    for(size_t j = 0 ; j < N ; ++j)
+    {
+        for(size_t i = 0 ; i < N ; ++i)
+        {
+            cov[ij] = variance * AmatrixT[ij] ; //  fjac[i+m*j]
+            ij++;
+        }
+    }
+
+    free(AmatrixT);
+    return;
+}
+
+void standardError(const size_t N, const double *cov, double* standardError)
+{
+    for(size_t i=0; i<N; ++i)
+    {
+        standardError[i] = sqrt( cov[ i + N * i ] );
+    }
+
+    return;
+}
+
+void printVariable(const double X1, const double X2, const double X3,
+                   const size_t X4)
+{
+    std::cout << "\n";
+    std::cout << X1 << "\t"<< X2 << "\t" << X3 << "\t" << X4;
+    std::cout << "\n";
+    return;
+}
+
+void printArray(const double* const matrix, const size_t N)
+{
+    std::cout << "\n";
+    for(size_t j = 0 ; j < N ; ++j)
+    {
+        std::cout<< matrix[j] << "\n";
+    }
+    std::cout << "\n";
+    return;
+}
+
+void printArray(const std::vector<double> &matrix, const size_t N)
+{
+    std::cout << "\n";
+    for(size_t j = 0 ; j < N ; ++j)
+    {
+        std::cout<< matrix[j] << "\n";
+    }
+    std::cout << "\n";
+    return;
+}
+
+void printArray2d(const size_t N, const size_t M, const double* const matrix)
+{
+    for(size_t j = 0 ; j < M ; ++j)
+    {
+        for(size_t i = 0 ; i < N ; ++i)
+        {
+            std::cout<< matrix[i+N*j] << "\t";
+        }
+        std::cout << "\n";
+    }
+    return;
+}
+
+void printArray2d(const size_t N, const size_t M,
+                  const std::vector<std::vector<double>>& matrix)
+{
+    for(size_t j = 0 ; j < M ; ++j)
+    {
+        for(size_t i = 0 ; i < N ; ++i)
+        {
+            std::cout<< matrix[j][i]<< "\t";
+        }
+        std::cout << "\n";
+    }
+    return;
+}
+
+void Amatrix(const size_t P, const size_t N, const double*fjac, double* Amatrix)
+{
+    /*
+        The A matrix is defined as the transpose of the jacobian dot product with
+        the jacobian matrix.  The jacobian matrix is of size (N,P).  The resulting
+        A matrix will be size (N,N). Where,
+            N is the number of parameters to be estimated
+            P is the number of measurements used in the estimation algorithm.
+    */
+    double*fjacT;
+    fjacT = (double*)malloc(P * N * sizeof(double));
+
+    matrixTranspose(P, N, fjac, fjacT);
+    matrixDot(fjacT, N, P, fjac, P, N, Amatrix);
+
+    free(fjacT);
+    return;
+}
+
+
+
+double MSEarea(const size_t N, std::vector<double> &func1,
+               std::vector<double> &func2)
+{
+    ///Startup and initialization
+    double sum = 0;
+    for(size_t i=0 ; i < N ; i++)
+    {
+        sum += fabs(func1[i] - func2[i]);
+    }
+
+    return sum / N;
+}
+
+double MSEarea1(size_t N, double* func1, double* func2, double* xvar)
+{
+/*
+  This function calculates the area between two curves.  This is done by
+  feeding in two lists of points (func1 and func2) along with the independent
+  variable (xvar).  The lists of points are fitted to an interpolating
+  cubic spline.  This spline is then used to evaluate the integral and area
+  between the lists.
+
+*/
+  ///Startup and initialization
+  using namespace math::numIntegration;
+  struct funcClass *Func1;
+  Func1 = new struct funcClass (xvar, func1, N);
+  struct funcClass *Func2;
+  Func2 = new struct funcClass (xvar, func2, N);
+
+  ///Evaluate Integration
+  double etol = 1e-5;
+  double xStep = etol;
+  double xo;
+  double area = 0;
+
+  for (double xi = xvar[0]; xi <= xvar[N-1]; xi += xStep)
+  {
+    xo = xi;
+    if(Func1->eval(xi) > Func2->eval(xi))
+    {
+      while(Func1->eval(xi) > Func2->eval(xi))
+      {
+          xi += xStep;
+      }
+      area += integrate(Func1, xo, xi) - integrate(Func2, xo, xi);
+      xi -= xStep;
+    }
+
+    else if(Func2->eval(xi) > Func1->eval(xi))
+    {
+      while(Func2->eval(xi) > Func1->eval(xi))
+      {
+          xi += xStep;
+      }
+      area += integrate(Func2, xo, xi) - integrate(Func1, xo, xi);
+      xi -= xStep;
+    }
+
+    else if( fabs(Func2->eval(xi) - Func1->eval(xi)) <  etol)
+    {
+      area +=0;
+    }
+  }
+
+  ///Cleanup
+  delete Func1;
+  delete Func2;
+
+  return area;
+}
+
+template< typename Type >
+constexpr Type maximum( Type a, Type b )
+{
+    return a < b ? b : a;
+}
+
+
---
+++ src/thermal/simulations/Numerical_PhaseOfEmission.cpp
@@ -0,0 +1,179 @@
+/*----------------------------------------------------------------------------*\
+  ========                      |
+     || 	 T Thermal      | TAT: Thermal Analysis Toolbox
+     ||  	 A Analysis     |
+     || 	 T Toolbox    	| Copyright (C) 2013 Raymond Valdes
+     ||   	  		|
+-------------------------------------------------------------------------------
+License
+    This file is part of Thermal Analysis Toolbox.
+
+    Thermal Analysis Toolbox is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    Thermal Analysis Toolbox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with
+    Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
+
+\*----------------------------------------------------------------------------*/
+#include "thermal/analysis/kernal.hpp"
+#include "thermal/simulations/Numerical_Temperature.h"
+#include "thermal/simulations/Numerical_PhaseOfEmission.h"
+#include "math/statistical_tools.hpp"
+#include "thermal/simulations/Numerical_Setup.h"
+#include "thermal/emission.hpp"
+#include "thermal/model.hpp"
+#include "thermal/heat/heat2DAna.hpp"
+#include "thermal/thermal.hpp"
+
+double PhaseOfEmission1DNum( const double omega,
+                             const class thermal::analysis::Kernal &popteaCore)
+{
+  class Temperature Tprofile( popteaCore.thermalsys.mesh.Nend,
+                              popteaCore.thermalsys.mesh.M2);
+
+  //// Acquire Numerical Temperature
+//  const double omega = omegas[flag];
+  temperature_1D( popteaCore.TBCsystem,
+                  popteaCore.thermalsys,
+                  popteaCore.expSetup, omega, Tprofile);
+
+  ///Initiate emission model
+  const double Lcoat = popteaCore.TBCsystem.coating.depth;
+  const class thermal::Emission emission( popteaCore.expSetup.detector.wavelength,
+                                          popteaCore.TBCsystem.Temp.base,
+                                          popteaCore.thermalsys.mesh,
+                                          popteaCore.bEval() *Lcoat,
+                                          popteaCore.TBCsystem.optical.Emit1 );
+
+  const double phase2 = emission.phase1D( Tprofile );
+  Tprofile.cleanup();
+
+  return phase2;
+}
+
+double PhaseOfEmission2DAna( const double omega,
+                             const class thermal::analysis::Kernal &popteaCore)
+{
+    /// Initiate 2d analytical model
+    const double k_coat  = popteaCore.TBCsystem.coating.kthermal.offset;
+    const double psi_coat= popteaCore.TBCsystem.coating.psithermal.offset;
+    const double Lcoat = popteaCore.TBCsystem.coating.depth;
+    const double csub = popteaCore.TBCsystem.substrate.c;
+    const double ccoat = popteaCore.TBCsystem.coating.c;
+
+    const class HeatModel2DAna
+        heatmodel2DAna( popteaCore.TBCsystem.optical.R0,
+                        popteaCore.TBCsystem.optical.R1,
+                        popteaCore.TBCsystem.coating.lambda,
+                        popteaCore.expSetup.laser.It,
+                        csub, popteaCore.TBCsystem.a_subEval(),
+                        popteaCore.TBCsystem.gammaEval(),
+                        popteaCore.bEval(), k_coat, Lcoat, psi_coat, ccoat,
+                        popteaCore.TBCsystem.substrate.lambda,
+                        popteaCore.thermalsys.mesh) ;
+
+    /// Setup Temperature[n][r][z] Vector
+    std::vector< std::vector< std::vector< double > > > T2DProfile;
+    vector3DSetup(T2DProfile, popteaCore.thermalsys.mesh.Nend,
+                  popteaCore.thermalsys.mesh.Rend,
+                  popteaCore.thermalsys.mesh.M2);
+
+    ///Populate Temperature Vector
+    heatmodel2DAna.TemperatureDistro(T2DProfile, omega );
+
+    ///Initiate emission model
+    const class thermal::Emission emission(popteaCore.expSetup.detector.wavelength,
+                                  popteaCore.TBCsystem.Temp.referance,
+                                  popteaCore.thermalsys.mesh,
+                                  popteaCore.bEval() * Lcoat,
+                                  popteaCore.TBCsystem.optical.Emit1
+                                  );
+
+    const double phase2d = emission.phase2D(T2DProfile);
+    return phase2d;
+}
+
+
+double PhaseOfEmission1DAna(const double omega1,
+                            const thermal::analysis::Kernal &popteaCore)
+{
+  /*See 2004 emission paper equation 19*/
+//  const double omega1 = omegas[flag];
+  const double lambda = popteaCore.TBCsystem.coating.lambda;
+  const double Esigma = popteaCore.TBCsystem.optical.Emit1;
+  const double gamma  = popteaCore.TBCsystem.gammaEval();
+  const double R1     = popteaCore.TBCsystem.optical.R1;
+  const double L_coat = popteaCore.TBCsystem.coating.depth;
+  const double k_c    = popteaCore.TBCsystem.coating.kthermal.offset;
+  const double psi_c  = popteaCore.TBCsystem.coating.psithermal.offset;
+  const double l = thermal::lthermal(L_coat,k_c,psi_c,omega1);
+
+  constexpr std::complex<double> _i_ (0.0, 1.0);
+
+  std::complex<double>
+  M = 1;
+  M -= sqrt(_i_) * lambda / l * sinh( sqrt(_i_)/l ) / sinh( 1/lambda ) ;
+  M /=  gamma * cosh( sqrt(_i_)/l ) + sinh( sqrt(_i_)/l ) ;
+
+  std::complex<double>
+  N = 1 ;
+  N -= _i_ * lambda * lambda / l / l * cosh( sqrt(_i_)/l ) / cosh( 1/lambda ) ;
+  N /= gamma * cosh( sqrt(_i_)/l ) + sinh( sqrt(_i_)/l ) ;
+
+  std::complex<double>
+  A =  1 + R1 ;
+  A *= 1 - exp(-2/lambda);
+  A *= M;
+
+  std::complex<double>
+  B =  1 - R1 ;
+  B *= 1 + exp(-2/lambda) ;
+  B *= N;
+
+  const std::complex<double>
+  v = Esigma * sqrt(_i_) / l - 4 * gamma ;
+
+  std::complex<double>
+  u  = 2;
+  u *=  1. - lambda * lambda / l / l * _i_  ;
+
+  const std::complex<double>
+  t = A + B;
+
+  double
+  phase_emit_ana_C = std::arg( v/u*t + 4 * ( 1. - R1 * exp(-2/lambda) ) ) ;
+  phase_emit_ana_C -= M_PI_2;
+
+  return phase_emit_ana_C;
+}
+
+
+
+Temperature::Temperature(const size_t Nend_, const size_t M2_)
+: Nend(Nend_), M2(M2_)
+{
+  temperature = new double[Nend_ * M2_];
+}
+
+double Temperature::eval(const size_t i, const size_t j) const
+{
+  return temperature[j + M2 * i];
+}
+
+void Temperature::assgn(const size_t i, const size_t j, const double value)
+{
+  temperature[j + M2 * i] = value;
+}
+
+void Temperature::cleanup(void)
+{
+  delete[]temperature;
+}
+
