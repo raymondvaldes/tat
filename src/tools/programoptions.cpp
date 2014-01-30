5d7eb553c82ef71a2c8de0ab696bc1daaf8f598e
Raymond Valdes <raymondvaldes@gmail.com>
2014-01-29 22:06:47 -0800

    update program options

--- src/tools/programoptions.cpp
+++ src/tools/programoptions.cpp
@@ -36,45 +36,72 @@ namespace programoptions{
 bool loadOptions( int argc, char** argv, std::string& path )
 {
   namespace po = boost::program_options;
-
   po::options_description desc("Allowed options");
   desc.add_options()
       ("help", "produce help message")
-      ("config,c", po::value<std::string>(&path),"set path to config folder")
+      ("config,c", po::value<std::string>(&path),
+        "set path to config files dir")
       ("version,v", "display version information")
+      ("build,b","build libraries")
       ;
-
+  bool run = false;
   po::variables_map vm;
-  po::store( po::command_line_parser( argc, argv ).options( desc ).run(), vm ) ;
+
+  try
+  {
+    po::store( po::command_line_parser( argc,argv ).options(desc).run(), vm ) ;
+  }
+  catch (std::exception const&  ex)
+  {
+    std::cout << "See help below...\n";
+    version();
+    run = help(desc);
+  }
+
   po::notify( vm ) ;
 
   if (vm.count("help"))
-  {
-    std::cout << desc << "\n";
-    return false;
-  }
+    { run = help(desc) ; }
 
   if (vm.count("config"))
-  {
-    std::cout << "path configured... " << path << "\n";
-    return true;
-  }
+    { run = config(path) ; }
 
   if (vm.count("version"))
-    { return version(); }
+    { run = version() ; }
 
+  if (vm.count("build"))
+    { run = build() ; }
+
+  return run;
+}
+
+bool help( boost::program_options::options_description desc)
+{
+  std::cout << desc << "\n";
+  return false;
+}
+
+bool config( std::string& path)
+{
+  std::cout << "path configured... " << path << "\n";
   return true;
 }
 
+
 bool version(void)
 {
-  std::cout << "tat (Ubuntu) \n" ;
+  std::cout << "TAT: " ;
   std::cout << "Thermal Analysis Toolbox\n";
   std::cout << "Copyright (C) 2013 Raymond Valdes\n\n" ;
   std::cout << "This is free software; see the source for copying " ;
   std::cout << "conditions.  There is NO\n" ;
   std::cout << "warranty; not even for MERCHANTABILITY or FITNESS FOR A" ;
   std::cout << " PARTICULAR PURPOSE.\n\n" ;
+  return false;
+}
+
+bool build(void)
+{
   std::cout << "Built with:\n";
   std::cout << "Boost "
             << BOOST_VERSION / 100000     << "."  // major version
@@ -85,8 +112,7 @@ bool version(void)
             << GSL_MAJOR_VERSION  << "."          // minior version
             << GSL_MINOR_VERSION                  // patch level
             << std::endl;
-  std::cout << "Written by Raymond Valdes.\n";
-return false;
+  return false;
 }
 
 
