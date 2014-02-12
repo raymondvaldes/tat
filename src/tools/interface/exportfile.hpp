8c5f5edad4e553259b65e38b5df9ddaa14b0e4c5
Raymond Valdes <raymondvaldes@gmail.com>
2014-02-12 10:47:28 -0800

    exportfile
    Create a method to input a string with a file path string and it exports
    the data into a file.  Easier I/O.

--- src/tools/interface/exportfile.hpp
+++ src/tools/interface/exportfile.hpp
@@ -1,10 +1,10 @@
 /*----------------------------------------------------------------------------*\
-  ========                      |
-     || 	 T Thermal      | TAT: Thermal Analysis Toolbox
-     ||  	 A Analysis     |
-     || 	 T Toolbox    	| Copyright (C) 2013 Raymond Valdes
-     ||   	  		|
--------------------------------------------------------------------------------
+  ========                  |
+     ||     T Thermal       | TAT: Thermal Analysis Toolbox
+     ||     A Analysis      |
+     ||     T Toolbox       | Copyright (C) 2013 Raymond Valdes
+     ||                     |
+--------------------------------------------------------------------------------
 License
     This file is part of Thermal Analysis Toolbox.
 
@@ -15,22 +15,27 @@ License
 
     Thermal Analysis Toolbox is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+    Public License for more details.
 
     You should have received a copy of the GNU General Public License along with
     Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
 
 \*----------------------------------------------------------------------------*/
-#ifndef EXPORT_HPP
-#define EXPORT_HPP
 
-namespace tools
-{
+#ifndef EXPORTFILE_HPP
+#define EXPORTFILE_HPP
 
-double Print_Emission( const double*phase, const double*num_phase,
-                       const double*ana_phase,
-                       const double*l_thermal, const size_t L_end);
-}
+#include <string>
 
-#endif // EXPORT_HPP
+namespace tools{
+namespace interface{
+
+
+void exportfile( const std::string fullpath, const std::string input ) ;
+
+
+
+}}
+
+#endif // EXPORTFILE_HPP
