84ec5d91cb13994af1536d9ac4b3479a0631e2a5
Raymond Valdes <raymondvaldes@gmail.com>
2014-02-12 14:07:41 -0800

    added definition to disable assert (globaly)

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,8 @@ License
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <cassert>
+//#define NDEBUG
 
 //Internal dependencies
 #include "tools/tools.hpp"
@@ -49,7 +51,6 @@ int main( int argc, char *argv[ ] )
     /// Run investigations
 //    investigations::sensitivityvaldes2013::demo( dir );
     investigations::sensitivityvaldes2013::run( dir );
-
     //investigations::manyfit::run( dir );
 
     std::cout << "\nStopwatch: "                << std::setprecision(3)
