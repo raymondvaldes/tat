0652bfbeceb9ab7bd623a42776c80bd31b4d8c9d
Raymond Valdes <raymondvaldes@gmail.com>
2014-03-19 14:19:23 -0700

    removed nominal emission from thermalData class

--- src/thermal/analysis/thermalData.hpp
+++ src/thermal/analysis/thermalData.hpp
@@ -43,7 +43,6 @@ public:
   std::vector<double> omegas;
   std::vector<double> experimentalEmission;
   std::vector<double> predictedEmission;
-  std::vector<double> nominalEmission;
   std::pair<double, double> lthermalPredicted;
   size_t measurementsPerDecade;
   double MSE;
