63e0f37a8acebb6fb7c2488e86c60e69d0ba6891
Raymond Valdes <raymondvaldes@gmail.com>
2014-04-25 14:55:21 -0700

    moved spline class

---
+++ src/math/algorithms/spline.cpp
@@ -0,0 +1,83 @@
+
+/*----------------------------------------------------------------------------*\
+ ========                 |
+    || 	   T Thermal      | TAT: Thermal Analysis Toolbox
+    ||  	 A Analysis     |
+    || 	   T Toolbox    	| Copyright (C) 2013 Raymond Valdes
+    ||   	  	          	|
+ -------------------------------------------------------------------------------
+ License
+ This file is part of Thermal Analysis Toolbox.
+ 
+ Thermal Analysis Toolbox is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or (at your
+ option) any later version.
+ 
+ Thermal Analysis Toolbox is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along with
+ Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
+ 
+\*----------------------------------------------------------------------------*/
+
+#include "math/algorithms/spline.h"
+#include <cmath>
+
+namespace math{
+namespace algorithms{
+
+mySpline::mySpline( const double *a, const double *b, const size_t c )
+  :
+  xvar(a), func(b), N(c),
+  spline( gsl_spline_alloc( gsl_interp_cspline, N ) ),
+  workspace( gsl_integration_workspace_alloc (limit) )
+{
+  /*The arguments include two arrays where
+      a ...independent array
+      b ...dependent array
+      c ...size of array */
+  ///alloc space for the mySpline anbd assign to pointer
+  gsl_spline_init( spline, xvar, func, N);
+}
+
+mySpline::~mySpline(void)
+{
+  gsl_spline_free( spline ) ;
+  gsl_interp_accel_free( acc ) ;
+  gsl_integration_workspace_free( workspace ) ;
+}
+
+
+double mySpline::eval( const double xpos ) const
+{
+  using std::abs;
+
+  double xposNew = xpos;
+  if( abs( xpos - xvar[0]   ) < tol ) xposNew = xvar[0] + tol;
+  if( abs( xpos - xvar[N-1] ) < tol ) xposNew = xvar[N-1] - tol;
+
+
+  if( xposNew < xvar[0] || xposNew > xvar[N-1] )
+  {
+    std::cout << "outside range!!\n\n"
+    <<xpos<<" is outside of range "<<xvar[0]<<"\t"<<xvar[N-1]<<"\n";
+    exit(-71);
+    return 0;
+  }
+
+  return gsl_spline_eval( spline, xposNew, acc );
+}
+
+double mySpline::CCallback( double d, void*params )
+{
+  return static_cast<mySpline*>(params)->eval(d);
+}
+
+
+
+}
+}
---
+++ src/math/algorithms/spline.h
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*\
+ ========                 |
+    || 	   T Thermal      | TAT: Thermal Analysis Toolbox
+    ||  	 A Analysis     |
+    || 	   T Toolbox    	| Copyright (C) 2013 Raymond Valdes
+    ||   	  	          	|
+ -------------------------------------------------------------------------------
+ License
+ This file is part of Thermal Analysis Toolbox.
+ 
+ Thermal Analysis Toolbox is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or (at your
+ option) any later version.
+ 
+ Thermal Analysis Toolbox is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along with
+ Thermal Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
+ 
+\*----------------------------------------------------------------------------*/
+
+#ifndef __tat__mySpline_math__algorithms__
+#define __tat__mySpline_math__algorithms__
+
+#include <iostream>
+#include <vector>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_integration.h>
+
+namespace math{
+namespace algorithms{
+
+class mySpline
+{
+private:
+  static constexpr size_t limit = 1001;    //defines size of workspace
+  static constexpr double tol = 1e-14;
+  const double* xvar;
+  const double* func;
+  const size_t  N;
+
+  gsl_interp_accel *acc   = gsl_interp_accel_alloc();  ///Ptr interpolation iter
+  gsl_spline *spline      = nullptr; ///Declare pointer to interpolations
+  gsl_integration_workspace *workspace  = nullptr;
+  static double CCallback(double d, void*params);
+
+public:
+  explicit mySpline( const double* a, const double* b, const size_t c ) ;
+  ~mySpline( void ) ;
+  
+  double eval( const double xpos ) const;
+};
+
+}
+}
+
+#endif /* defined(__tat__mySpline_math__algorithms__) */
