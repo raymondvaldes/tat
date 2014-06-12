f1178afe1408d741f7faf8c7944e691f6d6ba8dd
Raymond Valdes <raymondvaldes@gmail.com>
2014-06-12 15:26:11 -0700

    reformatted test code into lambda functions

--- tat-test/math/utility/test_utility.cpp
+++ tat-test/math/utility/test_utility.cpp
@@ -62,17 +62,20 @@ BOOST_AUTO_TEST_CASE( within_tolerance ) {
   using math::within_tolerance;
   
   constexpr double tol = 0.0001 ;
-  constexpr double A1 = -10.00 ;
-  constexpr double A2 = -10.1 ;
 
-  const double forwarddiff = abs( A1 - A2 ) ;
-  const double backwarddiff = abs( A2 - A1 ) ;
-  
-  const bool trueAnswer1 = forwarddiff < tol ;
-  const bool trueAnswer2 = backwarddiff < tol ;
+  const auto within_tol_checker = [=]( const double val, const double val2 ) {
+    const double diff = abs( val - val2 ) ;
+    const bool trueAnswer = diff < tol ;
+    const bool pass = within_tolerance( val, val2, tol ) == trueAnswer ;
+    return pass ;
+  } ;
 
-  BOOST_CHECK( within_tolerance( A1, A2, tol ) == trueAnswer1 ) ;
-  BOOST_CHECK( within_tolerance( A2, A1, tol ) == trueAnswer2 ) ;
+  BOOST_CHECK( within_tol_checker( -10.0, -10.000001 ) ) ;  // close numbers
+  BOOST_CHECK( within_tol_checker( -10.1, 0 ) ) ;           // zero with neg
+  BOOST_CHECK( within_tol_checker( 5, 6 ) ) ;               // diff int
+  BOOST_CHECK( within_tol_checker( -5, 6 ) ) ;              // neg,pos
+  BOOST_CHECK( within_tol_checker( 105.12, 105.12 ) ) ;     // same double
+  BOOST_CHECK( within_tol_checker( 6, 6 ) ) ;               // same int
 }
 
 BOOST_AUTO_TEST_CASE( average_of_all ) {
@@ -82,38 +85,28 @@ BOOST_AUTO_TEST_CASE( average_of_all ) {
   using math::within_tolerance;
   
   constexpr size_t size = 99 ;
+  constexpr double tol_rel = .02 ;
+
+  const auto avg_checker = [ = ]( const double test_case ) {
+    vector<double> scalefunc( size ) ;
+
+    for( size_t i = 0 ; i < size ; ++i ) {
+      scalefunc[i] = ( double(i) / ( size - 1 ) ) * test_case ;
+    }
+    
+    const double calc_avg = average_of_all( scalefunc.data(), scalefunc.size());
+    const double true_avg = test_case / 2 ;
+    const double tol_abs = abs( true_avg ) * tol_rel ;
+    
+    const bool pass = within_tolerance( calc_avg, true_avg, tol_abs ) ;
+    return pass ;
+  } ;
   
-  constexpr double caseA = 24 ;
-  constexpr double caseB = 0.24 ;
-  constexpr double caseC = -.50 ;
-  constexpr double tol = .02 ;
-
-  vector<double> caseAarray(size) ;
-  vector<double> caseBarray(size) ;
-  vector<double> caseCarray(size) ;
-  for( size_t i = 0 ; i < size ; ++i ) {
-    caseAarray[i] = ( double(i) / ( size - 1 ) ) * caseA ;
-    caseBarray[i] = ( double(i) / ( size - 1 ) ) * caseB ;
-    caseCarray[i] = ( double(i) / ( size - 1 ) ) * caseC ;
-
-  }
-
-  const double true_avg_A = caseA / 2 ;
-  const double true_avg_B = caseB / 2 ;
-  const double true_avg_C = caseC / 2 ;
-  
-  const double calc_avg_A = average_of_all(caseAarray.data(),caseAarray.size());
-  const double calc_avg_B = average_of_all(caseBarray.data(),caseBarray.size());
-  const double calc_avg_C = average_of_all(caseCarray.data(),caseCarray.size());
-
-  const bool caseAtest = within_tolerance(calc_avg_A,true_avg_A,abs(true_avg_A*tol));
-  const bool caseBtest = within_tolerance(calc_avg_B,true_avg_B,abs(true_avg_B*tol));
-  const bool caseCtest = within_tolerance(calc_avg_C,true_avg_C,abs(true_avg_C*tol));
-
-
-  BOOST_CHECK_MESSAGE( caseAtest , true_avg_A << calc_avg_A ) ;
-  BOOST_CHECK_MESSAGE( caseBtest , true_avg_B << calc_avg_B ) ;
-  BOOST_CHECK_MESSAGE( caseCtest , true_avg_C << calc_avg_C ) ;
-
+  BOOST_CHECK( avg_checker( 24 ) ) ;
+  BOOST_CHECK( avg_checker( 0.24 ) ) ;
+  BOOST_CHECK( avg_checker( -.50 ) ) ;
+  BOOST_CHECK( avg_checker( 3 ) ) ;
+  BOOST_CHECK( avg_checker( -4 ) ) ;
+  BOOST_CHECK( avg_checker( .0001 ) ) ;
 }
 
