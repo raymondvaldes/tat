a5dacf7100a1ee9f2832416ddfff702f1e908160
Raymond Valdes <raymondvaldes@gmail.com>
2014-06-24 12:42:58 -0700

    created unit test

---
+++ test/algorithm/string/split.cpp
@@ -0,0 +1,23 @@
+//
+//  split_T.cpp
+//  tat-test
+//
+//  Created by Raymond Valdes on 6/23/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#define BOOST_TEST_NO_LIB
+#include <boost/test/unit_test.hpp>
+
+#include "algorithm/string/split.h"
+
+BOOST_AUTO_TEST_SUITE( algorithm )
+BOOST_AUTO_TEST_SUITE( string )
+
+BOOST_AUTO_TEST_CASE( split_name ) {
+
+  BOOST_REQUIRE(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
