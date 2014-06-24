d4d742814670df5ed618a50a1257741151ee95b4
Raymond Valdes <raymondvaldes@gmail.com>
2014-06-24 12:54:31 -0700

    created unit test framework

---
+++ test/algorithm/string/starts_with.cpp
@@ -0,0 +1,23 @@
+//
+//  starts_with.cpp
+//  tat-test
+//
+//  Created by Raymond Valdes on 6/24/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#define BOOST_TEST_NO_LIB
+#include <boost/test/unit_test.hpp>
+
+#include "algorithm/string/starts_with.h"
+
+BOOST_AUTO_TEST_SUITE( algorithm )
+BOOST_AUTO_TEST_SUITE( string2 )
+
+BOOST_AUTO_TEST_CASE( split_name ) {
+
+  BOOST_REQUIRE(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
