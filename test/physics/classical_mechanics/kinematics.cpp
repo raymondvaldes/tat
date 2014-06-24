4c2fb2dee57b79888387c10edebd5e6bc5028efe
Raymond Valdes <raymondvaldes@gmail.com>
2014-06-24 12:41:43 -0700

    created test file

---
+++ test/physics/classical_mechanics/kinematics.cpp
@@ -0,0 +1,22 @@
+//
+//  kinematics.cpp
+//  tat-test
+//
+//  Created by Raymond Valdes on 6/24/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#define BOOST_TEST_NO_LIB
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE( physics )
+BOOST_AUTO_TEST_SUITE( classica_mechanics )
+
+BOOST_AUTO_TEST_CASE( split ) {
+
+  BOOST_REQUIRE(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
