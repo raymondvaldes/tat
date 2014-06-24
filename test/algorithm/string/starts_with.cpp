36b1e1a0736683246415256c78423bf71a2bbef2
Raymond Valdes <raymondvaldes@gmail.com>
2014-06-24 16:04:05 -0700

    test unit

--- test/algorithm/string/starts_with.cpp
+++ test/algorithm/string/starts_with.cpp
@@ -10,14 +10,38 @@
 #include <boost/test/unit_test.hpp>
 
 #include "algorithm/string/starts_with.h"
+using algorithm::string::starts_with;
 
 BOOST_AUTO_TEST_SUITE( algorithm )
-BOOST_AUTO_TEST_SUITE( string2 )
 
-BOOST_AUTO_TEST_CASE( split_name ) {
+struct Access {
+  const std::string singleword = "pizza" ;
+  const std::string directory = "/home/user/file/stuff.cpp" ;
+  const std::string add_words = "alpha+bravo+charlie+delta" ;
+  const std::string spaced_words = " one two three four five" ;
+  const std::string numbers_delimiters = "0::0::0::0::0::::" ;
+};
 
-  BOOST_REQUIRE(true);
+BOOST_FIXTURE_TEST_SUITE( string_starts_with, Access )
+
+BOOST_AUTO_TEST_CASE( string_starts_with ) {
+
+  const bool pass = starts_with( singleword, "pi" ) ;
+  BOOST_CHECK( pass ) ;
 }
+
+BOOST_AUTO_TEST_CASE( numbers_delimiters ) {
+
+  const bool fail = starts_with( singleword, "::" ) ;
+  BOOST_CHECK( !fail ) ;
+}
+
+BOOST_AUTO_TEST_CASE( string_starts_with_space) {
+
+  const bool pass = starts_with( spaced_words, " one two " ) ;
+  BOOST_CHECK( pass ) ;
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
--- test/physics/classical_mechanics/kinematics.cpp
+++ test/physics/classical_mechanics/kinematics.cpp
@@ -8,14 +8,63 @@
 
 #define BOOST_TEST_NO_LIB
 #include <boost/test/unit_test.hpp>
-
+#include "physics/classical_mechanics/kinematics.h"
+using physics::classical_mechanics::period_to_angularFrequency;
+using physics::classical_mechanics::angularFrequency_to_period;
 
 BOOST_AUTO_TEST_SUITE( physics )
-BOOST_AUTO_TEST_SUITE( classica_mechanics )
 
-BOOST_AUTO_TEST_CASE( split ) {
 
-  BOOST_REQUIRE(true);
+class rotational_data {
+public:
+
+  class rotational_rate {
+  public:
+    const double period;
+    const double angularFreq;
+    rotational_rate( const double period_in, const double angularFreq_in )
+      : period( period_in), angularFreq( angularFreq_in )  {};
+  };
+
+  const rotational_rate true1 = rotational_rate( 6.2831853071795864  , 1 ) ;
+  const rotational_rate true2 = rotational_rate( 0.0172024238389584 , 365.25 ) ;
+  const rotational_rate true3 = rotational_rate( 1, 6.2831853071795864 ) ;
+
+  const double tol = 1e-12;
+  
+};
+
+typedef rotational_data::rotational_rate rate;
+rate calc_dependencies( const rate input )
+{
+  const double period_calc = angularFrequency_to_period( input.angularFreq ) ;
+  const double angularFreq_calc = period_to_angularFrequency( input.period ) ;
+
+  const rate output_calc( period_calc, angularFreq_calc );
+
+  return output_calc;
+};
+
+
+
+BOOST_FIXTURE_TEST_SUITE( classica_mechanics, rotational_data )
+
+
+BOOST_AUTO_TEST_CASE( angularFreq_to_period ) {
+
+  const rate calc_val1 = calc_dependencies( true1 ) ;
+  BOOST_CHECK_CLOSE( calc_val1.period, true1.period, tol ) ;
+  BOOST_CHECK_CLOSE( calc_val1.angularFreq, true1.angularFreq, tol ) ;
+
+  const rate calc_val2 = calc_dependencies( true2 ) ;
+  BOOST_CHECK_CLOSE( calc_val2.period, true2.period, tol ) ;
+  BOOST_CHECK_CLOSE( calc_val2.angularFreq, true2.angularFreq, tol ) ;
+
+  const rate calc_val3 = calc_dependencies( true3 ) ;
+  BOOST_CHECK_CLOSE( calc_val3.period, true3.period, tol ) ;
+  BOOST_CHECK_CLOSE( calc_val3.angularFreq, true3.angularFreq, tol ) ;
+
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
