62cf5625a332fe7443bcbd02ad5625d6282eeafa
Raymond Valdes <raymondvaldes@gmail.com>
2014-09-11 18:49:20 -0700

    Created function that adds two std::vectors

---
+++ src/algorithm/vector/add.cpp
@@ -0,0 +1,34 @@
+//
+//  add.cpp
+//  tat
+//
+//  Created by Raymond Valdes on 9/11/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#include "algorithm/vector/add.h"
+#include <boost/assert.hpp>
+
+#include <algorithm>    // std::transform
+#include <functional>   // std::plus
+
+namespace algorithm {
+namespace vector{
+
+std::vector<double> add( const std::vector<double>& firstvec,
+                         const std::vector<double>& secondvec){
+  using std::plus;
+  using std::transform;
+  
+  const size_t length = firstvec.size();
+  BOOST_ASSERT( length == secondvec.size() );
+
+  std::vector<double> result( length ) ;
+  
+  transform( firstvec.begin(), firstvec.end(),
+             secondvec.begin(),
+             result.begin(), std::plus<double>());
+
+  return result;
+}
+}}
---
+++ src/algorithm/vector/add.h
@@ -0,0 +1,23 @@
+//
+//  add.h
+//  tat
+//
+//  Created by Raymond Valdes on 9/11/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__add__
+#define __tat__add__
+
+#include <vector>
+
+namespace algorithm {
+namespace vector{
+
+std::vector<double> add( const std::vector<double>& foo,
+                         const std::vector<double>& bar);
+
+}}
+
+
+#endif /* defined(__tat__add__) */
