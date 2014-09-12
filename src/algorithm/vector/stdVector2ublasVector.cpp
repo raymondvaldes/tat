2c02f1d715619bf5b60c94c66a06ef3fe283e9bb
Raymond Valdes <raymondvaldes@gmail.com>
2014-09-11 18:51:15 -0700

    convert std::vector to boost::numerics::ublas::vector  and test code

---
+++ src/algorithm/vector/stdVector2ublasVector.cpp
@@ -0,0 +1,30 @@
+//
+//  stdVector2ublasVector.cpp
+//  tat
+//
+//  Created by Raymond Valdes on 9/11/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#include "algorithm/vector/stdVector2ublasVector.h"
+#include <boost/assert.hpp>
+#include <algorithm>
+
+namespace algorithm {
+namespace vector{
+
+boost::numeric::ublas::vector<double>
+stdVector2ublasVector( const std::vector<double>& input ) {
+
+  using boost::numeric::ublas::vector;
+  using std::copy;
+  
+  vector<double> output( input.size() );
+  
+  copy(input.begin(), input.end(), output.begin());
+
+  return output;
+}
+
+
+}}
---
+++ src/algorithm/vector/stdVector2ublasVector.h
@@ -0,0 +1,24 @@
+//
+//  stdVector2ublasVector.h
+//  tat
+//
+//  Created by Raymond Valdes on 9/11/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__stdVector2ublasVector__
+#define __tat__stdVector2ublasVector__
+
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <vector>
+
+namespace algorithm {
+namespace vector{
+
+boost::numeric::ublas::vector<double>
+stdVector2ublasVector( const std::vector<double>& input );
+
+
+}}
+#endif /* defined(__tat__stdVector2ublasVector__) */
