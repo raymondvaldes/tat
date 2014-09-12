39b5945955669e1171c012125849a52ffe90b7e0
Raymond Valdes <raymondvaldes@gmail.com>
2014-09-11 18:50:33 -0700

    function to subtract two vectors and test code

---
+++ src/algorithm/vector/subtract.cpp
@@ -0,0 +1,34 @@
+//
+//  subtract.cpp
+//  tat
+//
+//  Created by Raymond Valdes on 9/11/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#include "algorithm/vector/subtract.h"
+#include <boost/assert.hpp>
+
+#include <algorithm>    // std::transform
+#include <functional>   // std::plus
+
+namespace algorithm {
+namespace vector{
+
+std::vector<double> subtract(  const std::vector<double>& firstvec,
+                               const std::vector<double>& secondvec){
+  using std::transform;
+  using std::minus;
+  
+  const size_t length = firstvec.size();
+  BOOST_ASSERT( length == secondvec.size() );
+  std::vector<double> result( length ) ;
+  
+  transform( firstvec.begin(), firstvec.end(),
+             secondvec.begin(),
+             result.begin(), minus<double>() ) ;
+
+    return result;
+}
+
+}}
---
+++ src/algorithm/vector/subtract.h
@@ -0,0 +1,23 @@
+//
+//  subtract.h
+//  tat
+//
+//  Created by Raymond Valdes on 9/11/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__subtract__
+#define __tat__subtract__
+
+#include <vector>
+
+namespace algorithm {
+namespace vector{
+
+std::vector<double> subtract( const std::vector<double>& foo,
+                               const std::vector<double>& bar);
+
+}}
+
+
+#endif /* defined(__tat__subtract__) */
