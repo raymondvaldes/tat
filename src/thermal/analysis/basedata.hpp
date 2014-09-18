4d546aa1a93546c6d08195517bdf2ed0d3e077a8
Raymond Valdes <raymondvaldes@gmail.com>
2014-09-18 12:12:13 -0700

    added ability to update baseData objects and reassign pointers

--- src/thermal/analysis/basedata.hpp
+++ src/thermal/analysis/basedata.hpp
@@ -37,6 +37,24 @@ public:
   std::shared_ptr< Kernal > coreSystem;
   std::shared_ptr< ThermalData > thermalData;
   std::shared_ptr< math::estimation::unknownList > unknownParameters;
+  
+  void updateObjects( const Kernal& coreSystemIn, const ThermalData& thermalDataIn,
+               const math::estimation::unknownList& unknownParametersIn ) {
+    using std::make_shared;
+    
+    coreSystem = make_shared< Kernal >( coreSystemIn ) ;
+    thermalData = make_shared< ThermalData >( thermalDataIn ) ;
+    unknownParameters = make_shared< math::estimation::unknownList >( unknownParametersIn ) ;
+  };
+  
+  void assignPointer( const std::shared_ptr< math::estimation::unknownList > &list_in,
+         const std::shared_ptr< ThermalData > &thermalData_in,
+         const std::shared_ptr< thermal::analysis::Kernal > &coreSystem_in )
+  {
+    unknownParameters = list_in ;
+    thermalData = thermalData_in ;
+    coreSystem = coreSystem_in ;
+  };
 };
 
 }}
