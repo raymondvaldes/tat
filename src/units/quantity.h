6cb84fe375edfaf308e452d82d7ae257350c58a0
Raymond Valdes <raymondvaldes@gmail.com>
2014-10-02 22:44:40 -0700

    basic unit quantity

---
+++ src/units/quantity.h
@@ -0,0 +1,23 @@
+//
+//  quantity.h
+//  tat
+//
+//  Created by Raymond Valdes on 9/30/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef tat_quantity_h
+#define tat_quantity_h
+
+#include <boost/units/quantity.hpp>
+
+namespace units{
+
+
+
+template<class Unit,class Y = double>
+  using quantity = boost::units::quantity<Unit, Y>;
+
+}
+
+#endif
