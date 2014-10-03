1d3e5bb85eadcd41ebd3466c1491e96a1b87bf9e
Raymond Valdes <raymondvaldes@gmail.com>
2014-10-02 22:49:06 -0700

    approximation for plank Wien approximation

---
+++ src/thermal/emission/blackBody/planckWienApprox.cpp
@@ -0,0 +1,37 @@
+//
+//  planckWienApprox.cpp
+//  tat
+//
+//  Created by Raymond Valdes on 9/30/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#include <cmath>
+#include <boost/assert.hpp>
+#include "thermal/emission/blackBody/planckWienApprox.h"
+#include "thermal/emission/blackBody/constants.h"
+
+using std::pow;
+using std::exp;
+
+namespace thermal {
+namespace emission {
+namespace blackBody {
+  
+double plankWienApprox( const double wavelength, const double temperature ) {
+
+  BOOST_ASSERT( temperature > 0 );
+  BOOST_ASSERT( wavelength > 0 ) ;
+  
+  const double lambda = wavelength ;
+  const double Temp = temperature ;
+  
+  double
+  Ib  = C1 ;
+  Ib /= M_PI *  pow( lambda, 5 )  ;
+  Ib *= exp( -C2 / ( lambda * Temp ) ) ;
+  
+  return Ib;
+}
+
+}}}
---
+++ src/thermal/emission/blackBody/planckWienApprox.h
@@ -0,0 +1,22 @@
+//
+//  planckWienApprox.h
+//  tat
+//
+//  Created by Raymond Valdes on 9/30/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__planckWienApprox__
+#define __tat__planckWienApprox__
+
+namespace thermal {
+  namespace emission {
+  namespace blackBody {
+  
+  double plankWienApprox( const double wavelength, const double temperature ) ;
+  
+
+  
+  
+}}}
+#endif /* defined(__tat__planckWienApprox__) */
