c8a70fbbb104376d7d9245d2d2a866dbc02ea746
Raymond Valdes <raymondvaldes@gmail.com>
2014-10-02 22:47:23 -0700

    template that takes the ratio of two signals

---
+++ src/thermal/emission/signalRatio.h
@@ -0,0 +1,27 @@
+//
+//  signalRatio.h
+//  tat
+//
+//  Created by Raymond Valdes on 9/30/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__signalRatio__
+#define __tat__signalRatio__
+
+#include "thermal/emission/signal.h"
+#include "units/si/si.h"
+
+namespace thermal {
+  namespace emission {
+  
+template< class signalType >
+auto signalRatio( const Signal<signalType> first,
+                  const Signal<signalType> second ) ->
+  units::quantity< units::si::dimensionless >
+{
+  return second.getIntensity() / first.getIntensity() ;
+}
+  
+}}
+#endif /* defined(__tat__signalRatio__) */
