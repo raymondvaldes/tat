26179df6ec840b9ee9d43f058b836737870735d8
Raymond Valdes <raymondvaldes@gmail.com>
2014-10-02 22:44:15 -0700

    introduce si units

---
+++ src/units/si/predefined.h
@@ -0,0 +1,29 @@
+//
+//  length.h
+//  tat
+//
+//  Created by Raymond Valdes on 9/30/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__length__
+#define __tat__length__
+
+#include <boost/units/systems/si.hpp>
+
+namespace units {
+namespace si {
+
+typedef boost::units::si::length length;
+typedef boost::units::si::mass_density mass_density;
+typedef boost::units::si::mass mass;
+
+typedef boost::units::si::temperature temperature;
+typedef boost::units::si::power power;
+typedef boost::units::si::solid_angle solid_angle;
+typedef boost::units::si::electric_potential electric_potential;
+typedef boost::units::si::dimensionless dimensionless ;
+
+}}
+
+#endif /* defined(__tat__length__) */
