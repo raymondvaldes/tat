523e6b4d3b4abdc6738911acfd9ef2fffcf67dc1
Raymond Valdes <raymondvaldes@gmail.com>
2014-10-02 22:42:52 -0700

    namespace for thermal emission blackbody constant

---
+++ src/thermal/emission/blackBody/constants.h
@@ -0,0 +1,30 @@
+//
+//  constants.h
+//  tat
+//
+//  Created by Raymond Valdes on 9/30/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__constants__
+#define __tat__constants__
+
+#include "units/si/si.h"
+
+namespace thermal {
+  namespace emission {
+  namespace blackBody {
+  
+  constexpr double C1 = 59552197;    // [ W um^4 / m^2 ]
+  constexpr double C2 = 14387.6; // [ um K ]
+  
+  
+
+    
+}}}
+
+
+
+
+
+#endif /* defined(__tat__constants__) */
