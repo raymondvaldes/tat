9ee0d2b7eb423908ad91953b26ae0691ccef4a80
Raymond Valdes <raymondvaldes@gmail.com>
2014-10-02 22:48:46 -0700

    namespace for pyrometer object

---
+++ src/thermal/pyrometry/twoColor/pyrometer.h
@@ -0,0 +1,27 @@
+//
+//  pyrometer.h
+//  tat
+//
+//  Created by Raymond Valdes on 10/2/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef tat_pyrometer_h
+#define tat_pyrometer_h
+
+#include "thermal/pyrometry/twoColor/calibrationCoefficient.h"
+#include "thermal/pyrometry/twoColor/temperatureMeasurement.h"
+
+namespace thermal {
+  namespace pyrometer{
+  namespace twoColor{
+  
+  //const signal< units::si::electrical_potential>
+  
+  
+  
+}}}
+
+
+
+#endif
