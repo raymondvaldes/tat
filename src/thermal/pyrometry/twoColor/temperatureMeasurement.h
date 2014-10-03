279a4471f1041e7b0891fdc86d662a5e062964c0
Raymond Valdes <raymondvaldes@gmail.com>
2014-10-02 22:48:08 -0700

    necessary changes for private members

---
+++ src/thermal/pyrometry/twoColor/temperatureMeasurement.h
@@ -0,0 +1,13 @@
+//
+//  predictedTemperature.h
+//  tat
+//
+//  Created by Raymond Valdes on 10/2/14.
+//  Copyright (c) 2014 Raymond Valdes. All rights reserved.
+//
+
+#ifndef tat_predictedTemperature_h
+#define tat_predictedTemperature_h
+
+
+#endif
