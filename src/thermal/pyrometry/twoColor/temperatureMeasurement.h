17b9080206aadab2780f0b8c8b3a67ad7d222ca9
Raymond Valdes <raymondvaldes@gmail.com>
2014-12-18 13:50:15 -0800

    code to return temperature based on calibration information, signal ratio, and the two wavelengths

--- src/thermal/pyrometry/twoColor/temperatureMeasurement.h
+++ src/thermal/pyrometry/twoColor/temperatureMeasurement.h
@@ -9,5 +9,32 @@
 #ifndef tat_predictedTemperature_h
 #define tat_predictedTemperature_h
 
+#include "thermal/emission/signal.h"
+#include "units/si/si.h"
+#include "units/si/constants/thermal.h"
 
+namespace thermal{
+namespace pyrometer{
+namespace twoColor{
+
+auto temperatureSteady(   const units::quantity< units::si::dimensionless > calibrationCoefficient,
+                          const units::quantity< units::si::dimensionless > signalRatio,
+                          const units::quantity< units::si::length > wavelength_one,
+                          const units::quantity< units::si::length > wavelength_two
+                          ) -> units::quantity< units::si::temperature>
+{
+  using units::si::constants::C2_wien;
+  using std::log;
+  
+  const auto SR = signalRatio ;
+  const auto G = calibrationCoefficient;
+  
+  auto
+  temp = C2_wien / wavelength_one - C2_wien / wavelength_two ;
+  temp /= log( G * SR ) - 5 * log( wavelength_one / wavelength_two );
+
+  return temp;
+}
+
+}}}
 #endif
