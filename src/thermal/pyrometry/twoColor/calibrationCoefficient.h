8cf1202f9c728c88a2989af18ab42815dadf2864
Raymond Valdes <raymondvaldes@gmail.com>
2014-12-18 13:53:14 -0800

    added calibration coefficients

--- src/thermal/pyrometry/twoColor/calibrationCoefficient.h
+++ src/thermal/pyrometry/twoColor/calibrationCoefficient.h
@@ -9,8 +9,9 @@
 #ifndef __tat__calibrationCoefficient__
 #define __tat__calibrationCoefficient__
 
-#include "thermal/emission/signal.h"
 #include "units/si/si.h"
+#include "units/si/constants/thermal.h"
+#include "thermal/emission/signal.h"
 #include "thermal/emission/signalRatio.h"
 
 namespace thermal {
@@ -21,19 +22,27 @@ template< class signalType >
 auto calibrationCoefficient(
   const emission::Signal< signalType > first,
   const emission::Signal< signalType > second,
-  const units::quantity< units::si::temperature> Temp ) -> double
+  const units::quantity< units::si::temperature> Temp )
+  -> units::quantity< units::si::dimensionless >
 {
-
-
-  auto input = signalRatio( first, second ) ;
+  using units::si::constants::C2_wien;
+  using std::exp;
   
+  auto SR = signalRatio( first, second ) ;
+  const auto lambda_one = first.getwavelength();
+  const auto lambda_two = second.getwavelength();
   
   
   auto
-  output = signalRatio( first, second ) ;
+  output  =  C2_wien / lambda_one - C2_wien / lambda_two ;
+  output /= Temp;
   
-
-  return Temp.value();
+  output += 5 * log( lambda_two / lambda_one ) ;
+  output = exp( output ) ;
+  output /= SR ;
+  
+  
+  return output;
 };
 
 
