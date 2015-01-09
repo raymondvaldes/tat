4c6dc1f38aea233df70fb05f036de228485944f9
Raymond Valdes <raymondvaldes@gmail.com>
2015-01-09 13:50:12 -0800

    working debugger

---
+++ test/stuff/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stuff
+//
+//  Created by Raymond Valdes on 1/9/15.
+//  Copyright (c) 2015 Raymond Valdes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
