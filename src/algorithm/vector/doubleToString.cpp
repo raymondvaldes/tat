896e2532cd25daf35a213fa73fac398bab3ece3e
Raymond Valdes <raymondvaldes@gmail.com>
2015-02-20 10:13:53 -0800

    convert vector of double to vector of string

---
+++ src/algorithm/vector/doubleToString.cpp
@@ -0,0 +1,41 @@
+//
+//  doubleToString.cpp
+//  tat
+//
+//  Created by Raymond Valdes_New on 2/17/15.
+//  Copyright (c) 2015 Raymond Valdes. All rights reserved.
+//
+
+#include <algorithm>
+
+#include "algorithm/vector/doubleToString.h"
+
+namespace algorithm {
+
+namespace vector {
+
+auto doubleToString( std::vector<double> const &  input ) noexcept -> std::vector<std::string>
+{
+  using std::vector;
+  using std::string;
+  using std::transform;
+  using std::to_string;
+  using std::begin;
+  using std::end;
+
+  auto const N = input.size() ;
+  
+  auto output = vector< string >( N ) ;
+
+  transform( begin( input ), end( input ), begin( output ),
+  []( const auto& E)
+  {
+    return to_string( E )  ;
+  }  ) ;
+  
+  return output;
+}
+
+} // namespace vector
+  
+} // namespace algorithm
