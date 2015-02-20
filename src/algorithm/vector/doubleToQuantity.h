804873f3fb271037c2eebc896b41997eb50d7f78
Raymond Valdes <raymondvaldes@gmail.com>
2015-02-20 10:13:11 -0800

    function that takes a vector of doubles and converts it to a vector of quantity< TYPE >

---
+++ src/algorithm/vector/doubleToQuantity.h
@@ -0,0 +1,45 @@
+//
+//  doubleToQuantity.h
+//  tat
+//
+//  Created by Raymond Valdes_New on 2/17/15.
+//  Copyright (c) 2015 Raymond Valdes. All rights reserved.
+//
+
+#ifndef tat_doubleToQuantity_h
+#define tat_doubleToQuantity_h
+
+#include <vector>
+#include <algorithm>
+
+#include "units.h"
+
+namespace algorithm {
+
+namespace vector {
+
+template< typename UnitType, typename UnitDimension>
+auto doubleToQuantity( std::vector< double > const & input, UnitDimension const & myDim  )
+noexcept -> std::vector< units::quantity< UnitType > >
+{
+  using std::vector;
+  using std::transform;
+  using std::begin;
+  using std::end;
+
+  auto output = vector< units::quantity< UnitType > >( input.size() ) ;
+
+  transform( begin( input ), end( input ), begin( output ), [&]( const auto& val )
+  {
+    auto const val_Dimensioned = units::quantity< UnitType > ( val * myDim ) ;
+    return val_Dimensioned  ;
+  } );
+  
+  return output;
+}
+  
+} // namespace vector
+  
+} // namespace algorithm
+
+#endif
