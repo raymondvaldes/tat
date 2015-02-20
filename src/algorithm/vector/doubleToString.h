615f79038bbdf22bad86a79bed3100758fe8c4c8
Raymond Valdes <raymondvaldes@gmail.com>
2015-02-20 10:14:20 -0800

    head for vector of double to vector of string

---
+++ src/algorithm/vector/doubleToString.h
@@ -0,0 +1,25 @@
+//
+//  doubleToString.h
+//  tat
+//
+//  Created by Raymond Valdes_New on 2/17/15.
+//  Copyright (c) 2015 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__doubleToString__
+#define __tat__doubleToString__
+
+#include <string>
+#include <vector>
+
+namespace algorithm {
+
+namespace vector {
+
+auto doubleToString( std::vector<double> const &  doubles ) noexcept -> std::vector<std::string>;
+
+} // namespace vector
+  
+} // namespace algorithm
+
+#endif /* defined(__tat__doubleToString__) */
