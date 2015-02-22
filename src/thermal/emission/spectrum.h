d51b19cd5007c72cfdb28a60d06d623c285d7585
Raymond Valdes <raymondvaldes@gmail.com>
2015-02-22 09:01:38 -0800

    classes to determine the range of the signals

--- src/thermal/emission/spectrum.h
+++ src/thermal/emission/spectrum.h
@@ -20,21 +20,14 @@ namespace emission {
 
 template< typename SignalType >
 class Spectrum{
+
   using SignalList = std::vector< Signal< SignalType > >;
 
-  const units::quantity< units::absolute <units::si::temperature > > sourceTemperature;
+  units::quantity< units::absolute <units::si::temperature > > sourceTemperature;
   
 public:
   SignalList signals;
 
-  
-public:
-  explicit Spectrum(
-    SignalList const & inputSignals,
-    units::quantity< units::absolute <units::si::temperature > > const & sourceTemperatureIn )
-  noexcept : sourceTemperature( sourceTemperatureIn ), signals(inputSignals)
-  {} ;
-  
   explicit Spectrum(
     SignalList const & inputSignals,
     units::quantity< units::absolute<units::celsius::temperature> > const & sourceTemperatureIn
@@ -43,15 +36,77 @@ public:
   units::absolute_conv_C_to_K_type::convert( sourceTemperatureIn ))
   {}
 
+  explicit Spectrum(
+    SignalList const & inputSignals,
+    units::quantity< units::absolute <units::si::temperature > > const & sourceTemperatureIn )
+  noexcept : sourceTemperature( sourceTemperatureIn ), signals( inputSignals )
+  {
+    using std::is_sorted;
+    using std::sort;
+    using std::begin;
+    using std::end;
+    
+    auto const fail = !is_sorted( begin( signals ), end( signals ) ) ;
+    
+    if( fail ) {
+      sort( begin( signals ) , end( signals ) );
+    }
+
+  } ;
+  
 
   auto source_Temperature(void)
   noexcept -> units::quantity< units::si::temperature >
   {
     return sourceTemperature;
   }
+  
+  auto min( void ) const noexcept -> Signal< SignalType >
+  {
+    return signals.front();
+  }
+  
+  auto max( void ) const noexcept -> Signal< SignalType >
+  {
+    return signals.back();
+  }
+  
+  auto if_available( units::quantity< units::si::wavelength > input )
+  const noexcept
+  -> bool
+  {
+    using std::any_of;
+    using std::begin;
+    using std::end;
+    using units::abs;
+    
+    auto const pass = any_of( begin(signals), end(signals),
+    [&input]( auto const &val )
+    {
+      auto const value = 1e-14;
+      auto const tol = units::quantity< units::si::wavelength>::from_value( value ) ;
+      
+      return ( abs( val.getElectromagneticWavelength() - input ) < tol ) ;
+    } );
+    
+    return pass;
+  }
+  
+    //iterators
+    typedef typename std::vector< Signal< SignalType > >::iterator iterator;
+  
+    typedef typename std::vector< Signal< SignalType > >::const_iterator const_iterator;
+
+
+    iterator begin() { return signals.begin(); }
+
+    const_iterator begin() const { return signals.begin(); }
+
+    iterator end() { return signals.end(); }
+
+    const_iterator end() const { return signals.end(); }
+  
 };
-
-
 
   
 } // namespace emission
