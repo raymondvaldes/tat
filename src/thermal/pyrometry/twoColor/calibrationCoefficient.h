95a7f237c190433bc40c6e8ce5ee5f22b964b19c
Raymond Valdes <raymondvaldes@gmail.com>
2015-02-23 13:15:21 -0800

    now accounts for absolute temperature scale

--- src/thermal/pyrometry/twoColor/calibrationCoefficient.h
+++ src/thermal/pyrometry/twoColor/calibrationCoefficient.h
@@ -27,27 +27,27 @@ template< class signalType >
 auto calibrationCoefficient(
   const emission::Signal< signalType > first,
   const emission::Signal< signalType > second,
-  const units::quantity< units::si::temperature> Temp ) noexcept
+  const units::quantity< units::absolute< units::si::temperature > > Temp_Abs ) noexcept
   -> units::quantity< units::si::dimensionless >
 {
-  assert_positive( Temp ) ;
-
   using units::si::constants::C2_wien;
   using std::exp;
   using std::log;
   
-  const auto SR = signalRatio( second, first ) ;
-  const auto lambda_one = first.getElectromagneticWavelength();
-  const auto lambda_two = second.getElectromagneticWavelength();
+  auto const Temp =
+    units::quantity< units::si::temperature >::from_value( Temp_Abs.value() ) ;
+  
+  auto const SR = signalRatio( second, first ) ;
+  auto const lambda_one = first.getElectromagneticWavelength();
+  auto const lambda_two = second.getElectromagneticWavelength();
+  
   
   auto
-  gCoeff  =  C2_wien / lambda_one - C2_wien / lambda_two ;
-  gCoeff /= Temp;
-  
+  gCoeff  =  ( C2_wien / lambda_one - C2_wien / lambda_two ) / Temp ;
+
   gCoeff += 5 * log( lambda_two / lambda_one ) ;
   gCoeff = exp( gCoeff ) ;
   gCoeff /= SR ;
-  
   
   return gCoeff;
 };
