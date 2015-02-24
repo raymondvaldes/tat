b68f40f92347d734575c10800eb3fb10f6df7fe3
Raymond Valdes <raymondvaldes@gmail.com>
2015-02-24 10:24:42 -0800

    fixed signal ratio issue

--- src/thermal/pyrometry/twoColor/calibrationCoefficient.h
+++ src/thermal/pyrometry/twoColor/calibrationCoefficient.h
@@ -25,27 +25,29 @@ namespace twoColor {
 
 template< class signalType >
 auto calibrationCoefficient(
-  const emission::Signal< signalType > first,
-  const emission::Signal< signalType > second,
-  const units::quantity< units::absolute< units::si::temperature > > Temp_Abs ) noexcept
+  emission::Signal< signalType > const & first,
+  emission::Signal< signalType > const & second,
+  units::quantity< units::absolute< units::si::temperature > > const & Temp_Abs )
+  noexcept
   -> units::quantity< units::si::dimensionless >
 {
   using units::si::constants::C2_wien;
+  using units::quantity;
+  using units::si::temperature;
   using std::exp;
   using std::log;
   
-  auto const Temp =
-    units::quantity< units::si::temperature >::from_value( Temp_Abs.value() ) ;
-  
-  auto const SR = signalRatio( second, first ) ;
+  auto const Temp = quantity< temperature >::from_value( Temp_Abs.value() ) ;
+  auto const SR = signalRatio( first, second ) ;
   auto const lambda_one = first.getElectromagneticWavelength();
   auto const lambda_two = second.getElectromagneticWavelength();
   
+ // std::cout << SR <<"\t" << C2_wien <<"\t"<< lambda_one << "\t" << lambda_two << "\t" << Temp << "\n";
   
   auto
   gCoeff  =  ( C2_wien / lambda_one - C2_wien / lambda_two ) / Temp ;
 
-  gCoeff += 5 * log( lambda_two / lambda_one ) ;
+  gCoeff += 5 * log( lambda_one / lambda_two ) ;
   gCoeff = exp( gCoeff ) ;
   gCoeff /= SR ;
   
