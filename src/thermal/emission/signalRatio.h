eb8c776c77734a4de4e011e602cb1ef669c65028
Raymond Valdes <raymondvaldes@gmail.com>
2015-02-24 10:22:50 -0800

    brought definition in line with the convention that the wavelength of lambda_1 < lambda_2

--- src/thermal/emission/signalRatio.h
+++ src/thermal/emission/signalRatio.h
@@ -9,19 +9,27 @@
 #ifndef __tat__signalRatio__
 #define __tat__signalRatio__
 
+#include <cassert>
+
 #include "thermal/emission/signal.h"
 #include "units/si/si.h"
 
 namespace thermal {
+
 namespace emission {
   
-template< class signalType >
-auto signalRatio( const Signal<signalType> first,
-                  const Signal<signalType> second )
-  -> units::quantity< units::si::dimensionless >
+template< class signal_type >
+auto signalRatio( Signal< signal_type > const & first,
+                  Signal< signal_type > const & second )
+noexcept -> units::quantity< units::si::dimensionless >
 {
-  return first.getIntensity() / second.getIntensity() ;
+  assert( first < second );
+
+  return second.getIntensity() / first.getIntensity() ;
 }
   
-}}
+} // namespace emission
+
+} // namespace thermal
+
 #endif /* defined(__tat__signalRatio__) */
