18297e38b4dd151cdea1a5485d740df16dd7e99d
Raymond Valdes <raymondvaldes@gmail.com>
2015-03-09 11:37:35 -0700

    created extentsions to the trigonometric functions. Also i needed to explicitly include the dimensionless versions of the functions.

--- src/units/cmath.h
+++ src/units/cmath.h
@@ -9,9 +9,14 @@
 #ifndef tat_cmath_h
 #define tat_cmath_h
 
+#include <complex>
+
 #include <boost/units/cmath.hpp>
+#include <boost/units/dimensionless_quantity.hpp>
+#include "units/physical_dimensions.h"
 
 namespace units {
+
   using boost::units::isfinite;
   using boost::units::isinf;
   using boost::units::isnan;
@@ -51,11 +56,95 @@ namespace units {
   using boost::units::cos;
   using boost::units::sin;
   using boost::units::tan;
+  
   using boost::units::acos;
 
   using boost::units::asin;
   using boost::units::atan;
   using boost::units::atan2;
+  
+
+/// custom cmath stuff
+  using boost::units::dimensionless_quantity;
+
+
+template<class Y>
+typename dimensionless_quantity<si::system,Y>::type
+sinh(const quantity< si::plane_angle, Y > & theta )
+{
+    using std::sinh;
+    return sinh( theta.value() );
+}
+
+template<class Y>
+typename dimensionless_quantity<si::system,Y>::type
+sinh(const quantity< si::dimensionless, Y > & theta )
+{
+    return sinh( theta.value() );
+}
+
+
+/// sinh of theta in other angular units
+template<class System,class Y>
+typename dimensionless_quantity<System,Y>::type 
+sinh( const quantity<unit<plane_angle_dimension,System>,Y>& theta )
+{
+    return sinh( quantity<si::plane_angle,Y>(theta) );
+}
+
+
+/// cosh of theta in radians
+template<class Y>
+typename dimensionless_quantity<si::system,Y>::type 
+cosh(const quantity<si::plane_angle,Y> & theta )
+{
+    using std::cosh;
+    return cosh( theta.value() );
+}
+
+/// cosh of theta in dimensionless
+template<class Y>
+typename dimensionless_quantity<si::system,Y>::type 
+cosh(const quantity< si::dimensionless, Y > & theta )
+{
+    using std::cosh;
+    return cosh( theta.value() );
+}
+
+/// cos of theta in other angular units 
+template<class System,class Y>
+typename dimensionless_quantity<System,Y>::type 
+cosh( const quantity<unit<plane_angle_dimension,System>,Y>& theta )
+{
+    return cosh( quantity<si::plane_angle,Y>(theta) );
+}
+
+/// tanh of theta in radians
+template<class Y>
+typename dimensionless_quantity<si::system,Y>::type 
+tanh(const quantity<si::plane_angle,Y> & theta )
+{
+    using std::tanh;
+    return tanh( theta.value() );
+}
+
+/// tanh of theta in dimensionless
+template<class Y>
+typename dimensionless_quantity<si::system,Y>::type 
+tanh(const quantity< si::dimensionless, Y > & theta )
+{
+    using std::tanh;
+    return tanh( theta.value() );
+}
+
+/// tan of theta in other angular units 
+template<class System,class Y>
+typename dimensionless_quantity<System,Y>::type 
+tanh(const quantity<unit<plane_angle_dimension,System>,Y>& theta)
+{
+    return tanh( quantity<si::plane_angle,Y>(theta) );
+}
+  
   
 } // namespace units
 
