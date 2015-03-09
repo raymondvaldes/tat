fefe64a8d3c32f6c8d3b5e7d4461f170da31596d
Raymond Valdes <raymondvaldes@gmail.com>
2015-03-09 11:40:47 -0700

    created files for the analytical solution to the wave equation

--- src/math/differential/waveEquation/analytical.h
+++ src/math/differential/waveEquation/analytical.h
@@ -10,7 +10,7 @@
 #define __tat__analytical__
 
 #include <cmath>
-#include <complex>
+#include "units.h"
 
 namespace math {
 
@@ -18,11 +18,46 @@ namespace differential {
 
 namespace waveEquation {
 
-auto analytical_solution_00( double const x, std::complex<double> const Kappa )
-noexcept -> std::complex<double>;
+template< typename T, typename Y >
+auto analytical_solution_00( T const x, Y const Kappa )
+noexcept -> Y
+{
+  // eq:  s''(x) + (K^2) s(x) == 0
+  // BC:  s(0) == 0
+  // BC:  s(1) == 1
+
+  using std::sin;
+  auto const s_x = sin( Kappa * x ) / sin( Kappa ) ;
+  return s_x;
+}
   
-auto analytical_solution_10( double const x, std::complex<double> const Kappa )
-noexcept -> std::complex<double>;
+template< typename T, typename Y >
+auto analytical_solution_10(
+  T const x,
+  Y const Kappa,
+  T const A,
+  T const L_b )
+noexcept -> Y
+{
+  // eq:  s''(x) + (K^2) s(x) == 0
+  // BC:  s'(0) == A
+  // BC:  s(L_b)  == 0
+
+  using std::cosh;
+  using std::sinh;
+  using std::tanh;
+  using units::sinh;
+  using units::tanh;
+  using units::cosh;
+  
+  auto const kx = Kappa * x;
+  auto const kL = Kappa * L_b;
+
+
+  auto const s_x = ( A / Kappa ) * ( sinh( kx ) - tanh( kL ) * cosh( kx ) ) ;
+  
+  return s_x;
+}
   
 } // namespace waveEquation
   
