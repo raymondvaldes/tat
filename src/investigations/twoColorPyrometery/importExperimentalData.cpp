7a28d26300a15d35e41591a2c194bfa1898e1304
Raymond Valdes <raymondvaldes@gmail.com>
2015-03-11 16:41:11 -0700

    starting to populate my development runner

--- src/investigations/twoColorPyrometery/importExperimentalData.cpp
+++ src/investigations/twoColorPyrometery/importExperimentalData.cpp
@@ -9,6 +9,8 @@
 #include "investigations/twoColorPyrometery/importExperimentalData.h"
 #include "gTBC/gMeasure/scopeFile.h"
 #include "gTBC/gMeasure/get_signal_from_scope_file.h"
+#include "gTBC/gMeasure/read_scope_file.h"
+#include "gTBC/gMeasure/is_scope_file.h"
 
 namespace investigations {
 
@@ -17,7 +19,12 @@ namespace twoColorPyrometery {
 auto importExperimentalData( filesystem::directory const & dir ) -> void
 {
 
-
+  auto const directoryPath = dir.path() ;
+  auto const filePath = dir.filePath( "CrWO6_200F_feb26_4.6_2_0.dat" );
+  
+  using gTBC::gMeasure::read_scope_file;
+  read_scope_file( filePath ) ;
+  
 
 }
   
