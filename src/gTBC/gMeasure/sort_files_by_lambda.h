ff0ca5e4681e3c72904d880459f06df97af254f0
Raymond Valdes <raymondvaldes@gmail.com>
2015-03-12 13:51:33 -0700

    sorts the files by the wavelength

---
+++ src/gTBC/gMeasure/sort_files_by_lambda.h
@@ -0,0 +1,32 @@
+//
+//  sort_files_by_lambda.h
+//  tat
+//
+//  Created by Raymond Valdes_New on 3/12/15.
+//  Copyright (c) 2015 Raymond Valdes. All rights reserved.
+//
+
+#ifndef __tat__sort_files_by_lambda__
+#define __tat__sort_files_by_lambda__
+
+#include <vector>
+
+#include "gTBC/gMeasure/scopeFile.h"
+#include "units.h"
+
+namespace gTBC {
+
+namespace gMeasure {
+
+auto
+sort_files_by_lambda
+(
+  std::vector< ScopeFile > const & scope_files
+)
+noexcept -> std::vector< ScopeFile >;
+
+} // namespace gMeasure
+  
+} // namespace gTBC
+
+#endif /* defined(__tat__sort_files_by_lambda__) */
