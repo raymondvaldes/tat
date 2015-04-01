a6922dd462bb155355c48174495f647b497296f4
Raymond Valdes <raymondvaldes@gmail.com>
2015-04-01 10:40:55 -0700

    clean up

--- src/thermal/analysis/poptea_initialize.h
+++ src/thermal/analysis/poptea_initialize.h
@@ -9,17 +9,24 @@
 #ifndef __tat__poptea_initialize__
 #define __tat__poptea_initialize__
 
-#include <iostream>
 #include "thermal/analysis/poptea.hpp"
-
 
 namespace thermal{
 namespace analysis{
 
-Poptea initializePopTeawithNominalEmission( const filesystem::directory &dir ) noexcept;
-Poptea initializePopTeaAndLoadSimuEmission( const filesystem::directory &dir ) noexcept;
-Poptea initializePopTeawithExperimentalEmission( const filesystem::directory &dir ) noexcept;
+Poptea
+initializePopTeawithNominalEmission( const filesystem::directory &dir )
+noexcept;
+
+Poptea
+initializePopTeaAndLoadSimuEmission( const filesystem::directory &dir )
+noexcept;
+
+Poptea
+initializePopTeawithExperimentalEmission( const filesystem::directory &dir )
+noexcept;
   
-}}
+} // namespace analysis
+} // namespace thermal
 
 #endif /* defined(__tat__poptea_initialize__) */
