01644d3ecc8ce0a6b42c3b50047e334a2a8f66c6
Raymond Valdes <raymondvaldes@gmail.com>
2015-05-06 11:36:47 -0700

    residual square implemented

--- src/statistics/signal_processing/residuals/residual_square.h
+++ src/statistics/signal_processing/residuals/residual_square.h
@@ -1,56 +1,56 @@
-////
-////  residual_square.h
-////  tat
-////
-////  Created by Raymond Valdes on 5/6/15.
-////  Copyright (c) 2015 Raymond Valdes. All rights reserved.
-////
 //
-//#ifndef tat_statistics_signal_processing_residuals_residual_square_h
-//#define tat_statistics_signal_processing_residuals_residual_square_h
+//  residual_square.h
+//  tat
 //
-//#include <cassert>
-//#include <cmath>
+//  Created by Raymond Valdes on 5/6/15.
+//  Copyright (c) 2015 Raymond Valdes. All rights reserved.
 //
-//#include "units.h"
-//#include "statistics/signal_processing/residuals/residual.h"
-//
-//namespace statistics{
-//namespace signal_processing{
-//
-//template< typename T >
-//auto residual_square(
-//  units::quantity< T > const observation,
-//  units::quantity< T > const model
-//) noexcept
-//{
-////  using std::isfinite;
-////  using namespace units;
-////  
-////  assert( isfinite( observation.value() ) );
-////  assert( isfinite( model.value() ) );
-////  
-////  auto const rs = pow<2>( residual( observation, model ) );
-////  
-////  return rs;
-//}
-//
-//template< typename T >
-//auto residual_square( T const observation, T const model ) noexcept
-//{
-////  using std::isfinite;
-////  using namespace units;
-////  using std::pow;
-////  
-////  assert( isfinite( observation ) );
-////  assert( isfinite( model ) );
-////  
-////  auto const rs = pow( residual( observation, model ) , 2 );
-////  
-////  return rs;
-//}
-//
-//}
-//}
-//
-//#endif
+
+#ifndef tat_statistics_signal_processing_residuals_residual_square_h
+#define tat_statistics_signal_processing_residuals_residual_square_h
+
+#include <cassert>
+#include <cmath>
+
+#include "units.h"
+#include "statistics/signal_processing/residuals/residual.h"
+
+namespace statistics{
+namespace signal_processing{
+
+template< typename T >
+auto residual_square(
+  units::quantity< T > const observation,
+  units::quantity< T > const model
+) noexcept
+{
+  using std::isfinite;
+  using namespace units;
+  
+  assert( isfinite( observation.value() ) );
+  assert( isfinite( model.value() ) );
+  
+  auto const rs = pow<2>( residual( observation, model ) );
+  
+  return rs;
+}
+
+template< typename T >
+auto residual_square( T const observation, T const model ) noexcept
+{
+  using std::isfinite;
+  using namespace units;
+  using std::pow;
+  
+  assert( isfinite( observation ) );
+  assert( isfinite( model ) );
+  
+  auto const rs = pow( residual( observation, model ) , 2 );
+  
+  return rs;
+}
+
+}
+}
+
+#endif
--- test/statistics/residual_analysis/residual_square_test.cpp
+++ test/statistics/residual_analysis/residual_square_test.cpp
@@ -7,49 +7,49 @@
 //
 
 
-//#include <boost/test/unit_test.hpp>
-//#include <boost/test/execution_monitor.hpp>
-//
-//#include "statistics/signal_processing/residuals/residual_square.h"
-//#include "units.h"
-//
-//using statistics::signal_processing::residual_square;
-//using namespace units;
-//
-//BOOST_AUTO_TEST_SUITE( statistics )
-//BOOST_AUTO_TEST_SUITE( signal_processing )
-//BOOST_AUTO_TEST_SUITE( residual_analysis )
-//
-//BOOST_AUTO_TEST_CASE( residual_square_test_1 )
-//{
-//  auto const a = 2;
-//  auto const b = 3;
-//
-//  auto const res = residual_square( a, b );
-//  
-//  BOOST_CHECK_EQUAL( res, 1 );
-//}
-//
-//BOOST_AUTO_TEST_CASE( residual_square_double )
-//{
-//  auto const a = 2.2;
-//  auto const b = 3.3;
-//
-//  auto const res = residual_square( a, b );
-//  
-//  BOOST_CHECK_CLOSE_FRACTION( res, 1.21, 1e-15 );
-//}
-//
-//BOOST_AUTO_TEST_CASE( residual_square_quantity )
-//{
-//  auto const a = quantity<plane_angle>( 2.2 * radians );
-//  auto const b = quantity<plane_angle>( 3.3 * radians );
-//
-//  auto const res = residual_square( a, b );
-//  
-//  BOOST_CHECK_CLOSE_FRACTION( res.value(), 1.21, 1e-15 );
-//}
-//
-//BOOST_AUTO_TEST_SUITE_END()
-//BOOST_AUTO_TEST_SUITE_END()
-//BOOST_AUTO_TEST_SUITE_END()
+#include <boost/test/unit_test.hpp>
+#include <boost/test/execution_monitor.hpp>
+
+#include "statistics/signal_processing/residuals/residual_square.h"
+#include "units.h"
+
+using statistics::signal_processing::residual_square;
+using namespace units;
+
+BOOST_AUTO_TEST_SUITE( statistics )
+BOOST_AUTO_TEST_SUITE( signal_processing )
+BOOST_AUTO_TEST_SUITE( residual_analysis )
+
+BOOST_AUTO_TEST_CASE( residual_square_test_1 )
+{
+  auto const a = 2;
+  auto const b = 3;
+
+  auto const res = residual_square( a, b );
+  
+  BOOST_CHECK_EQUAL( res, 1 );
+}
+
+BOOST_AUTO_TEST_CASE( residual_square_double )
+{
+  auto const a = 2.2;
+  auto const b = 3.3;
+
+  auto const res = residual_square( a, b );
+  
+  BOOST_CHECK_CLOSE_FRACTION( res, 1.21, 1e-15 );
+}
+
+BOOST_AUTO_TEST_CASE( residual_square_quantity )
+{
+  auto const a = quantity<plane_angle>( 2.2 * radians );
+  auto const b = quantity<plane_angle>( 3.3 * radians );
+
+  auto const res = residual_square( a, b );
+  
+  BOOST_CHECK_CLOSE_FRACTION( res.value(), 1.21, 1e-15 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
