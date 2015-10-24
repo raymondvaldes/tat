f22c3a3e796f96288a6824406be38e89eeee858e
Raymond Valdes <raymondvaldes@gmail.com>
2015-10-24 15:43:39 -0700

    initial commit

---
+++ src/thermal/analysis/oneLayer2D/amplitude_analysis/make_minimization_equation.cpp
@@ -0,0 +1,9 @@
+//
+//  make_minimization_equation.cpp
+//  tat
+//
+//  Created by Raymond Valdes on 10/24/15.
+//  Copyright © 2015 Raymond Valdes. All rights reserved.
+//
+
+#include "make_minimization_equation.hpp"
---
+++ src/thermal/analysis/oneLayer2D/amplitude_analysis/make_minimization_equation.hpp
@@ -0,0 +1,14 @@
+//
+//  make_minimization_equation.hpp
+//  tat
+//
+//  Created by Raymond Valdes on 10/24/15.
+//  Copyright © 2015 Raymond Valdes. All rights reserved.
+//
+
+#ifndef make_minimization_equation_hpp_13hiuh34fi
+#define make_minimization_equation_hpp_13hiuh34fi
+
+#include <stdio.h>
+
+#endif /* make_minimization_equation_hpp */
