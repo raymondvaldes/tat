de4be7e6df9eb236f5f89808e1e608d69082be6c
Raymond Valdes <raymondvaldes@gmail.com>
2015-10-24 14:34:12 -0700

    initial commit to move general code out of specific analysis

---
+++ src/thermal/analysis/oneLayer2D/theoretical_modeling.cpp
@@ -0,0 +1,18 @@
+//
+//  theoretical_modeling.cpp
+//  tat
+//
+//  Created by Raymond Valdes on 10/24/15.
+//  Copyright © 2015 Raymond Valdes. All rights reserved.
+//
+
+#include "theoretical_modeling.hpp"
+
+namespace thermal{
+namespace analysis{
+namespace oneLayer2D {
+
+
+} // namespace oneLayer2D
+} // namespace analysis
+} // namespace thermal
