2ee8d9dc72cbecd68a62474fb9993a56018b2ad4
Raymond Valdes <raymondvaldes@gmail.com>
2015-10-24 20:56:01 -0700

    ability to choose fitting parameter

---
+++ src/thermal/analysis/oneLayer2D/fit_selection.h
@@ -0,0 +1,20 @@
+//
+//  fit_selection.h
+//  tat
+//
+//  Created by Raymond Valdes on 10/24/15.
+//  Copyright © 2015 Raymond Valdes. All rights reserved.
+//
+
+#ifndef fit_selection_h_102415
+#define fit_selection_h_102415
+
+namespace thermal{
+namespace analysis{
+namespace oneLayer2D{
+
+enum class Fit_selection{ phase, temperature_amplitudes };
+
+}}}
+
+#endif /* fit_selection_h */
